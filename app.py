ac231433c54119a265ad15b6c65257f2824b9f07
Dieimp <mraingue@gmail.com>
2020-10-23 23:43:07 -0300

    Update app.py

--- app.py
+++ app.py
@@ -13,16 +13,18 @@ def Fibonacci():
     c = [0]
     #chega ao numero de termos que � a
     if n > 1:
-        c.append(a)
+        print(a)
         for i in range (2, n +1):
             b = b + a
             a = b - a
-            c.append(a)
+            print(a)
     else:
-        c.append(a)
-    return c
+        print(a)
+    
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
     
+    
+ 
