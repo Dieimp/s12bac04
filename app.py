34ed39461130ecafefd2d0140148368c878af51a
Dieimp <mraingue@gmail.com>
2020-10-23 23:16:02 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
-n = 50
-a = 1
-b = 1
-#chega ao numero de termos que � a
-if n > 1:
-    print(a)
-    for i in range (2, n +1):
-        b = b + a
-        a = b - a
+import os
+from flask import Flask, jsonify, request
+from math import sqrt
+
+app = Flask(__name__)
+
+@app.route('/')
+def fibonnaci():
+    n = 50
+    a = 1
+    b = 1
+    #chega ao numero de termos que � a
+    if n > 1:
         print(a)
-else:
-    print(a)
+        for i in range (2, n +1):
+            b = b + a
+            a = b - a
+            print(a)
+    else:
+        print(a)
+    return 
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+    
