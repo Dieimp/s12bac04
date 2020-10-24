f74b6e5c225a0290b63737fe48fd6305268a294c
Dieimp <mraingue@gmail.com>
2020-10-23 23:38:29 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,23 +5,22 @@ from math import sqrt
 app = Flask(__name__)
 
 @app.route('/')
-def helloworld():
-    return 'hello world'
-# def fibonnaci():
-#     n = 50
-#     a = 1
-#     b = 1
-#     c = [0]
-#     #chega ao numero de termos que � a
-#     if n > 1:
-#         c.append(a)
-#         for i in range (2, n +1):
-#             b = b + a
-#             a = b - a
-#             c.append(a)
-#     else:
-#         c.append(a)
-#     return c
+def Fibonacci():
+    # return 'hello world'
+    n = 50
+    a = 1
+    b = 1
+    c = [0]
+    #chega ao numero de termos que � a
+    if n > 1:
+        c.append(a)
+        for i in range (2, n +1):
+            b = b + a
+            a = b - a
+            c.append(a)
+    else:
+        c.append(a)
+    return c
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
