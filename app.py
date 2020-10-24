f2c876c4a98eef492d0ab97f1ab83b96797f4592
Dieimp <mraingue@gmail.com>
2020-10-23 23:22:39 -0300

    Update app.py

--- app.py
+++ app.py
@@ -9,18 +9,18 @@ def fibonnaci():
     n = 50
     a = 1
     b = 1
+    c = []
     #chega ao numero de termos que � a
     if n > 1:
-        print(a)
+        c.append(a)
         for i in range (2, n +1):
             b = b + a
             a = b - a
-            print(a)
+            c.append(a)
     else:
-        print(a)
-    return 
+        c.append(a)
+    return c
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
-    
